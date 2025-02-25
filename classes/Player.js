428fadd32cae1925e62ce9aa3a1fbdebfe0f2f1f
Bryan <bryanfinesw@gmail.com>
2025-02-25 11:45:30 -0500

    + Added Player class & Tailwind support

---
+++ classes/Player.js
@@ -0,0 +1,10 @@
+import Gameboard from './Gameboard.js';
+
+class Player {
+    constructor() {
+        this.board = new Gameboard();
+    };
+};
+
+// module.exports = Player;
+export default Player;
