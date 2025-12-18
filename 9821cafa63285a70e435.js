7b9e47ad07e9125ca3af03dc13e6378371d670f4
Bryan <bryanfinesw@gmail.com>
2025-12-18 14:28:44 -0500

    Deployment commit

---
+++ 9821cafa63285a70e435.js
@@ -0,0 +1,25 @@
+import "./styles.css";
+import { createGame } from "./modules/gameController";
+import { renderBoard } from "./modules/ui";
+import { bindGlobalEvents, bindBoardEvents } from "./modules/domEvents";
+
+
+const boards = document.querySelector(".boards");
+const playerBoard = document.createElement("div");
+const computerBoard = document.createElement("div");
+
+playerBoard.className = "player-board";
+computerBoard.className = "computer-board";
+
+boards.append(playerBoard, computerBoard);
+
+const game = createGame();
+game.startPlacement();
+
+renderBoard(game.player.gameboard, playerBoard);
+renderBoard(game.computer.gameboard, computerBoard, { hideShips: true });
+
+bindBoardEvents(computerBoard, game, true);
+bindBoardEvents(playerBoard, game, false);
+
+bindGlobalEvents(game);
